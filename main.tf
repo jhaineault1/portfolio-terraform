24188d3c5388543132a1fd7572ef24ea5492e9c9
jhaineault1 <jhaineault1@gmail.com>
2021-06-07 16:25:19 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,4 @@
-module "website" {
-  source  = "github.com/jhaineault1/portfolio-terraform"
-  version = "1.0.0"
+module "portfolio-terraform" {
+  source  = "git::https://github.com/jhaineault1/portfolio-terraform.git"
   domain = "portfolio.jeffhaineault.info"
 }
-# To create the resource,
-terraform apply  
-# To upload the static assests,
-aws s3 sync build/ s3:portfolio.jeffhaineault.info
