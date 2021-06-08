13c7c15246870a669927254e95141daf492680cf
jhaineault1 <jhaineault1@gmail.com>
2021-06-07 17:34:17 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,4 +2,6 @@ module "portfolio-terraform" {
   source  = "github.com/jhaineault1/portfolio-terraform.git"
   domain = "portfolio.jeffhaineault.info"
 }
+terraform apply
 
+aws s3 sync build/ s3:portfolio.jeffhaineault.info
